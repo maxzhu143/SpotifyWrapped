693c9ddaf1f6f0719c6ede4466d4ced7ed3870b0
Aiko <aikokjones@gmail.com>
2024-10-27 16:47:06 -0400

    improved appearances of login and register pages and made logout button direct to home page instead of logout.html

--- Wrappedapp/urls.py
+++ Wrappedapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),  # Dashboard route
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),    # Custom logout page
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),    # Custom logout page
     path('home/', views.home, name='home'),  # Home page route
     path('spotify-connect/', views.spotify_connect, name='spotify_connect'),
     path("your-django-endpoint/", views.my_data_view, name="my_data_view"),
