6716ecabf3b1d54e2189898569ba4f2d1fa63e5c
Max <mobs2005@gmail.com>
2024-11-03 01:13:20 -0400

    i really tried

--- Wrappedapp/models.py
+++ Wrappedapp/models.py
@@ -1,9 +1,19 @@
 """Model definitions for Wrappedapp."""
 
-from django.db import models
 
 # Define your models here if any exist
 # Example:
 # class YourModel(models.Model):
 #     """A description of what YourModel represents."""
 #     field_name = models.CharField(max_length=100)
+from django.db import models
+from django.contrib.auth.models import User
+
+class SpotifyAccount(models.Model) :
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    access_token = models.CharField (max_length=255)
+    refresh_token = models.CharField (max_length=255)
+    expires_at = models.DateTimeField ()
+    def __str__(self):
+        # Return a string representation, which can be the user's username
+        return self.user.username
