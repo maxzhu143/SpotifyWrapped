3b8a6eab0457b75d4c3c6845e15b56890c3ee4d1
TerryMooreV <terry.l.moore5@gmail.com>
2024-11-30 20:10:52 -0500

    Trying to add Heroku Implementation

--- SpotifyWrapped/settings.py
+++ SpotifyWrapped/settings.py
@@ -146,3 +146,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
