2fd24559a5d29a1eb9a4f3ce2f6af54b0f2e7dc9
TerryMooreV <terry.l.moore5@gmail.com>
2024-11-30 23:43:21 -0500

    Trying to add Heroku Implementation

--- SpotifyWrapped/settings.py
+++ SpotifyWrapped/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Environment variables
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
+DEBUG = True
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
