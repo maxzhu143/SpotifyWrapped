45c97042f841d5c7a124aefe61288b1a5ad1cb42
TerryMooreV <terry.l.moore5@gmail.com>
2024-12-01 04:26:21 -0500

    Remove all files in .gitignore from tracking

--- staticfiles/javascript/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.getElementById("myButton");
-    btn.addEventListener("click", async () => {
-        const response = await fetch("/your-django-endpoint/");
-        const data = await response.json();
-        console.log(data);
-    });
-});
